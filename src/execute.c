32616e238e0d5c74c976b3a0319c19b3d80a011c
Edouard Proust <eproust@car12s4.42barcelona.com>
2025-01-28 14:23:32 +0100

    update

--- src/execute.c
+++ src/execute.c
@@ -1,13 +1,28 @@
 #include "minishell.h"
 
-void	execute_command(t_cmd *cmd, int *pipefd, int haspipe)
+void	execute_command(t_cmd *cmd, int *pipefd)
 {
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+		ft_perror("fork"); // TODO (E) Leaks
+	if (pipefd != NULL)
+	{
+		dup2(); // TODO (E) check error
+		close(pipefd[1]);
+	}
 	if (cmd->infile != NULL)
 	{
 		if (access(cmd->infile) == -1)
-			ft_perror("access"); // TODO leaks
+			ft_perror("access"); // TODO (E) leaks
+		// open(); // TODO
 		dup2(cmd->infile, STDIN_FILENO);
 		close(cmd->infile);
+	}
+	if (cmd->outfile != NULL)
+	{
+
 	}
 }
 
@@ -21,15 +36,14 @@ int	execute_input(t_node *input)
 
 	while (input != NULL)
 	{
-		if (input->next !== NULL)
+		if (input->cmd->pipe_after)
 		{
 			if (pipe(pipefd) == -1)
 				ft_perror("pipe"); // TODO (E) Leaks
-			//child_pid = execute_command(??);
-			//close(pipefd[??]);
+			child_pid = execute_command(input->cmd, pipefd);
 		}
 		else
-		//	child_pid = execute_command(???);
+			child_pid = execute_command(input->cmd, NULL);
 		waitpid(child_pid, &status, 0);
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 			ft_perror("child status"); // TODO leaks
