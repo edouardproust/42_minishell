013b94b0527b3d459dea9ad303af1e14b75c6c65
Edouard Proust <eproust@cbr7s5.42barcelona.com>
2025-02-11 14:00:25 +0100

    working on remove node env

--- src/env/ops.c
+++ src/env/ops.c
@@ -18,12 +18,20 @@ void	envvar_addone(t_envvar **lst, t_envvar *new)
 	new->prev = tail;
 }
 
-/*
 void	envvar_removeone(t_envvar **lst, t_envvar *node)
 {
-
+	size_t	len = ft_strlen(node->name) + 1;
+	while (*lst)
+	{
+		if (ft_strncmp((*lst)->name, node->name, len) == 0)
+		{
+			// remove logic here
+		}
+		lst = lst->next;
+	}
 }
 
+/*
 void	envvar_updateone(t_envvar **lst, t_envvar *node)
 {
 
